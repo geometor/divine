0add24855b8247fc088c7d8970d30525a446e2fa
phiarchitect <phi@phiarchitect.com>
2025-10-24 17:06:20 -0700

    docs: update changelog for v0.2.4
    Grouping fix in the UI.

--- src/geometor/divine/__init__.py
+++ src/geometor/divine/__init__.py
@@ -4,5 +4,5 @@ from .divine import register_divine_hook
 __author__ = "geometor"
 __maintainer__ = "geometor"
 __email__ = "github@geometor.com"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __licence__ = "MIT"
